6a9956914e086c5ce6e8dd386c4d0de9240189a6
jhleeon <jhleon288@gmail.com>
2022-11-06 19:22:14 +0600

    backend & frontend templating

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AdminController extends Controller
+{
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function home()
+    {
+        return view('admin.home');
+    }
+
+    public function logout(){
+
+        Session::flush();
+        Auth::logout();
+        return redirect()->route('admin.login');
+
+    }
+}
